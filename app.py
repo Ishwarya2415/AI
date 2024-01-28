ae955c413000c61bdd2c26f797f8068085d37d36
Ishwarya2415 <157850850+Ishwarya2415@users.noreply.github.com>
2024-01-28 14:43:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
-  chat_app = ChatApp(st.secrets["OPENAI_API_KEY"])
+chat_app = ChatApp(st.secrets["OPENAI_API_KEY"])
+  
 # Sidebar contents
 with st.sidebar:
     st.title('🤗💬 LLM Chat App')
